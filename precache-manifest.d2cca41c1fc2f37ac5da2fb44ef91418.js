347bdb6eb0a2c09dfa96ed2914ff4ff821ef7ecb
Langford-tech <55645613+langfordquarshie21@users.noreply.github.com>
2020-11-12 14:18:56 +1300

    Add files via upload

---
+++ precache-manifest.d2cca41c1fc2f37ac5da2fb44ef91418.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55486f16df3e932a2ffe",
+    "url": "/css/app.b80e0eb8.css"
+  },
+  {
+    "revision": "45df6b55966537aaeb32b313f31ea74e",
+    "url": "/img/1.45df6b55.jpg"
+  },
+  {
+    "revision": "e8fe5cb09a26a3fdd3d92336b898a0d9",
+    "url": "/img/2.e8fe5cb0.jpg"
+  },
+  {
+    "revision": "f258baa328a38e681b19a8894680af32",
+    "url": "/img/3.f258baa3.jpg"
+  },
+  {
+    "revision": "43a4c2905559f8090f6fc3dc47055bfe",
+    "url": "/img/arrow-down.43a4c290.svg"
+  },
+  {
+    "revision": "5e9decf1660020cd5763af421b5a6ac8",
+    "url": "/img/arrow-up.5e9decf1.svg"
+  },
+  {
+    "revision": "8e319f5d74a3b74b939b7498bf4931d0",
+    "url": "/img/back.8e319f5d.svg"
+  },
+  {
+    "revision": "59f4726a98e1230f2ff661afaf1d048a",
+    "url": "/img/message.59f4726a.svg"
+  },
+  {
+    "revision": "7252ce7a9cbb18cb4172b0bcb8943c47",
+    "url": "/img/share.7252ce7a.svg"
+  },
+  {
+    "revision": "904c2ef9af8ffa9d2c2da8b634bbd549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d9461ee58fcd6eb0b5",
+    "url": "/js/about.2e661b5a.js"
+  },
+  {
+    "revision": "55486f16df3e932a2ffe",
+    "url": "/js/app.64920312.js"
+  },
+  {
+    "revision": "566cf5a8cdc871cf31f0",
+    "url": "/js/chunk-vendors.8e7ca923.js"
+  },
+  {
+    "revision": "b7601f3862a3144c156e69a9dd6d4984",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
